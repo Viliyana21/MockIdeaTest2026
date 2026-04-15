a8d83b36e078e6629c4186ee81f6d7c81bc55f7c
Viliyana21 <viliyana.kirilova@gmail.con>
2026-04-15 14:50:46 +0300

    first added test

---
+++ UnitTest1.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Net;
+using System.Text.Json;
+using RestSharp;
+using RestSharp.Authenticators;
+using MockTestIdea.models;   // това се подменя с името на папката, която ще се създаде
+
+
+
+namespace MockTestIdea
+{
+
+        [TestFixture]
+        public class Tests
+        {
+            private RestClient client;
+            private static string lastCreateadIdeaId;
+
+            private const string BaseUrl = "http://144.91.123.158:82"; //сменя се с необходимия
+            private const string StaticToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJKd3RTZXJ2aWNlQWNjZXNzVG9rZW4iLCJqdGkiOiJjMTk5NDFlZC0yYjkzLTQ5NDYtOTRlYS04ZmQyODVkMzdmMDgiLCJpYXQiOiIwNC8xNS8yMDI2IDEwOjQ4OjE1IiwiVXNlcklkIjoiOTA5MDQyMDEtOGI3OS00M2M3LTUzOTUtMDhkZTc2YTJkM2VjIiwiRW1haWwiOiJnb29kam9iQHNvZnR1bmkub3JnIiwiVXNlck5hbWUiOiJTZWNvbmRUcnkiLCJleHAiOjE3NzYyNzE2OTUsImlzcyI6IklkZWFDZW50ZXJfQXBwX1NvZnRVbmkiLCJhdWQiOiJJZGVhQ2VudGVyX1dlYkFQSV9Tb2Z0VW5pIn0.CfUn5VKgxZI8JNfRIrJvT0ka32tcPpLMDqqEtgyM1OE"; //след като е генериран от постман се поставя тук
+
+            private const string LoginEmail = "goodjob@softuni.org"; // да си сложа мои автентични тестови данни
+            private const string LoginPassword = "12345678";
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            string jwtToken;
+
+            if (!string.IsNullOrWhiteSpace(StaticToken))
+            {
+                jwtToken = StaticToken;
+            }
+            else
+            {
+                jwtToken = GetJwtToken(LoginEmail, LoginPassword);
+            }
+
+            var options = new RestClientOptions(BaseUrl)
+            {
+                Authenticator = new JwtAuthenticator(jwtToken)
+            };
+
+            this.client = new RestClient(options);
+        }
+
+        private string GetJwtToken(string email, string password)
+        {
+            var tempClient = new RestClient(BaseUrl);
+            var request = new RestRequest("/api/User/Authentication", Method.Post);
+            request.AddJsonBody(new { email, password });
+
+            var response = tempClient.Execute(request);
+
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                var content = JsonSerializer.Deserialize<JsonElement>(response.Content);
+                var token = content.GetProperty("token").GetString();
+
+                if (string.IsNullOrWhiteSpace(token))
+                {
+                    throw new InvalidOperationException("Token not found in the response.");
+                }
+                return token;
+            }
+            else
+            {
+                throw new InvalidOperationException($"Failed to authenticate. Status code: {response.StatusCode}, Response: {response.Content}");
+            }
+        }
+
+        [Order(1)]
+        [Test]
+        public void CreateIdea_WithRequiredFIelds_ShouldReturnSuccess() // smenq se imeto na testa
+        {
+            var ideaData = new IdeaDTO //smenq se imeto na var
+            {
+                Title = "Test Idea",
+                Description = "This is a test idea description.", /// внимателно се гледа от swagger
+                Url = ""
+            };
+
+            var request = new RestRequest("/api/Idea/Create", Method.Post); // vnimava se s naklonenite cherti (swagger)
+            request.AddJsonBody(ideaData);
+
+            var response = this.client.Execute(request);
+
+            var createResponse = JsonSerializer.Deserialize<ApiResponseDTO>(response.Content);
+
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK), "Expected status code 200 OK.");
+            Assert.That(createResponse.Msg, Is.EqualTo("Successfully created!"));
+
+        }
+
+        [Order(2)]
+        [Test]
+        public void GetAllIdeas_ShouldReturnSuccess()
+        {
+            var request = new RestRequest("/api/Idea/All", Method.Get);
+            var response = this.client.Execute(request);
+
+            var responseItems = JsonSerializer.Deserialize<List<ApiResponseDTO>>(response.Content);
+
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK), "Expected status code 200 OK.");
+            Assert.That(responseItems, Is.Not.Empty);
+            Assert.That(responseItems, Is.Not.Null);
+
+            lastCreateadIdeaId = responseItems.LastOrDefault()?.Id;
+
+        }
+        [Order(3)]
+        [Test]
+
+        public void EditExistingIdea_ShouldReturnSuccess()
+        {
+            var editRequestData = new IdeaDTO
+            {
+                Title = "Edited Idea",
+                Description = "This is a edited idea description.",
+                Url = ""
+            };
+
+
+            var request = new RestRequest("/api/Idea/Edit", Method.Put);
+
+            request.AddQueryParameter("ideaId", lastCreateadIdeaId);
+            request.AddJsonBody(editRequestData);
+
+            var response = this.client.Execute(request);
+
+            var editResponse = JsonSerializer.Deserialize<ApiResponseDTO>(response.Content);
+
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK), "Expected status code 200 OK.");
+            Assert.That(editResponse.Msg, Is.EqualTo("Edited successfully"));
+        }
+
+        [Order(4)]
+        [Test]
+
+        public void DeleteIdea_ShouldReturnSuccess()
+        {
+            var request = new RestRequest("/api/Idea/Delete", Method.Delete);
+            request.AddQueryParameter("ideaId", lastCreateadIdeaId);
+            var response = this.client.Execute(request);
+
+
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK), "Expected status code 200 OK.");
+            Assert.That(response.Content, Is.EqualTo("\"The idea is deleted!\""));
+        }
+
+        [Order(5)]
+        [Test]
+        public void CreateIdea_WithMissingRequiredFields_ShouldReturnBadRequest()
+        {
+            var ideaData = new IdeaDTO
+            {
+                Title = "",
+                Description = "This is a test idea description.",
+                Url = ""
+            };
+            var request = new RestRequest("/api/Idea/Create", Method.Post);
+            request.AddJsonBody(ideaData);
+
+            var response = this.client.Execute(request);
+
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.BadRequest), "Expected status code 400 Bad Request.");
+        }
+        [Order(6)]
+        [Test]
+
+        public void EditNonExistingIdea_ShouldReturnNotFound()
+        {
+            string nonExistingIdeaId = "9999999";
+            var editRequestData = new IdeaDTO
+            {
+                Title = "Edited Idea",
+                Description = "This is a edited idea description.",
+                Url = ""
+            };
+            var request = new RestRequest("/api/Idea/Edit", Method.Put);
+            request.AddQueryParameter("ideaId", nonExistingIdeaId);
+            request.AddJsonBody(editRequestData);
+
+            var response = this.client.Execute(request);
+
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.BadRequest), "Expected status code 400 Bad Request.");
+        }
+        [Order(7)]
+        [Test]
+
+        public void DeleteNonExistingIdea_ShouldReturnNotFound()
+        {
+            string nonExistingIdeaId = "9999999";
+
+            var request = new RestRequest("/api/Idea/Delete", Method.Delete);
+            request.AddQueryParameter("ideaId", nonExistingIdeaId);
+            var response = this.client.Execute(request);
+
+
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.BadRequest), "Expected status code 400 Bad Request.");
+            Assert.That(response.Content, Is.EqualTo("\"There is no such idea!\""));
+        }
+
+        [Test]
+        public void TestToFail()
+        {
+            string nonExistingIdeaId = "9999999";
+
+            var request = new RestRequest("/api/Idea/Delete", Method.Delete);
+            request.AddQueryParameter("ideaId", nonExistingIdeaId);
+            var response = this.client.Execute(request);
+
+
+
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.BadRequest), "Expected status code 400 Bad Request.");
+
+        }
+
+
+        [OneTimeTearDown]
+         public void TearDown()
+        {
+            this.client?.Dispose();
+}
+    }
+}
---
+++ models/ApiResponseDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+
+
+namespace MockTestIdea.models
+{
+    internal class ApiResponseDTO
+    {
+        [JsonPropertyName("msg")]
+
+        public string? Msg { get; set; }
+
+        [JsonPropertyName("id")]
+        public string? Id { get; set; }
+    }
+}
+
+
